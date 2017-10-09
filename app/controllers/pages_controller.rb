34256b4e52c910c8356ba8b44df9fc54e32fc955
DeadFred <f.cliff1@hotmail.com>
2017-10-09 14:20:38 +1000

    added messaging system and db

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,17 @@
 class PagesController < ApplicationController
+
+  def index
+    @post = Post.new
+  end
+
+  def save
+    @post = Post.new
+    @post.description = params[:message]
+    @post.save
+
+    if @post.save
+      redirect_to root_path
+    end
+  end
+
 end
